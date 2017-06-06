762f14ce421ac774fcd9ea21d49c9d9410c9470c
Anton Kapralov <a.e.kapralov@gmail.com>
2017-06-06 08:51:39 +0300

    Health integration test.

---
+++ transfer-rest-it/src/test/java/kae/demo/transferrest/api/it/HealthResourceIT.java
@@ -0,0 +1,30 @@
+package kae.demo.transferrest.api.it;
+
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.junit.JUnit4CitrusTestDesigner;
+import com.consol.citrus.message.MessageType;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ */
+public class HealthResourceIT extends JUnit4CitrusTestDesigner {
+
+  private static final String HEALTH_ENDPOINT = "health";
+
+  @Test
+  @CitrusTest
+  public void testGetKDKMissingHeader() throws Exception {
+    http().client(HEALTH_ENDPOINT)
+        .send()
+        .get();
+
+    http().client(HEALTH_ENDPOINT)
+        .receive()
+        .response(HttpStatus.OK)
+        .messageType(MessageType.JSON)
+        .payload("OK");
+  }
+
+}
